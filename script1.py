239b1658a92c7d05b421430e351ecd2a93cab5d4
khyatidahiya <110962637+khyatidahiya@users.noreply.github.com>
2022-08-17 00:00:47 -0700

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,8 @@
+
+n1=print(int(input('ENTER NUMBER 1 =')))
+n2=print(int(input('ENTER NUMBER 2 =')))
+
+if n1>n2:
+  print(n1,"is larger than",n2)
+else:
+  print(n2,"is larger than",n1)
---
+++ script2.py
@@ -0,0 +1,8 @@
+
+n3=int(input('ENTER NUMBER 1 ='))
+n4=int(input('ENTER NUMBER 2 ='))
+
+if n1<n2:
+  print(n1,"is smaller than",n2)
+else:
+  print(n2,"is smaller than",n1)
